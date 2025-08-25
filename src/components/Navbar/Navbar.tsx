c32dccc82afeb0465782b5d3c33ec7f81fea7568
tlhololaiza <tshwanetlholo7@gmail.com>
2025-08-25 12:28:46 +0200

    updated navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAppSelector, useAppDispatch } from '../../redux/hooks';
+import { logout } from '../../redux/authSlice';
+import './Navbar.css';
+
+const Navbar: React.FC = () => {
+  const { isAuthenticated, user } = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    localStorage.removeItem('user'); 
+    navigate('/login');
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">Shopping List</Link>
+      </div>
+      <ul className="navbar-links">
+         {isAuthenticated && user ? (
+          <>
+            <li>
+              <span>Hello, {user.name}</span>
+            </li>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <button onClick={handleLogout} className="logout-btn">
+                Logout
+              </button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
